08d6716885622ca82d4384c7e72538220a6b02cc
QuACS <eli@elischiff.org>
2020-08-08 17:30:33 +0000

    Sat Aug  8 17:30:33 UTC 2020

---
+++ precache-manifest.25e6bde917df06add2087f21bed5f922.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e931887a63de10c5100ce864cb8d0f",
+    "url": "/934216ff2abcdbec4102.worker.js"
+  },
+  {
+    "revision": "7b0329cab95241606ac5d5d463f30722",
+    "url": "/chunk-cfd8699e.934216ff2abcdbec4102.worker.js"
+  },
+  {
+    "revision": "5a090f8556d5d0369dfc",
+    "url": "/css/app.fe21dfb2.css"
+  },
+  {
+    "revision": "66e749cc94d5a65983cc",
+    "url": "/css/chunk-23e1614b.a29a3192.css"
+  },
+  {
+    "revision": "066f4530b0ee1c92440d",
+    "url": "/css/chunk-9d6fb3ca.27e1866e.css"
+  },
+  {
+    "revision": "ec50c5d4abb06c933626",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "767a48f0ff5e53735106b149ce26a801",
+    "url": "/docker-image.jpg"
+  },
+  {
+    "revision": "fadf000946f732f306bbe615ff152b3e",
+    "url": "/f4fa8c25e920d8916e13.module.wasm"
+  },
+  {
+    "revision": "0544db9197f51696206661b4dbc39cc5",
+    "url": "/img/quacs_logo_white_duck.0544db91.svg"
+  },
+  {
+    "revision": "cc0bc9f846d116e97d03bf4eaea5e39d",
+    "url": "/img/quacs_white.cc0bc9f8.svg"
+  },
+  {
+    "revision": "c6239e105982becbc06a147a0a966e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a090f8556d5d0369dfc",
+    "url": "/js/app.cb39915c.js"
+  },
+  {
+    "revision": "66e749cc94d5a65983cc",
+    "url": "/js/chunk-23e1614b.2d33454b.js"
+  },
+  {
+    "revision": "cc33322538ce5bed6907",
+    "url": "/js/chunk-2d0db20a.c550690a.js"
+  },
+  {
+    "revision": "af1b7ac7efed9858e740",
+    "url": "/js/chunk-2d0e59bc.97e32fad.js"
+  },
+  {
+    "revision": "4d6a27534e5febd7d590",
+    "url": "/js/chunk-2d20f2c7.d3edce14.js"
+  },
+  {
+    "revision": "9c9632d4a66d89424b9b",
+    "url": "/js/chunk-2d2169f0.998353af.js"
+  },
+  {
+    "revision": "066f4530b0ee1c92440d",
+    "url": "/js/chunk-9d6fb3ca.aebbd889.js"
+  },
+  {
+    "revision": "ec50c5d4abb06c933626",
+    "url": "/js/chunk-vendors.e473e42f.js"
+  },
+  {
+    "revision": "93b702478aef9bcd91184e1b555d7666",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
